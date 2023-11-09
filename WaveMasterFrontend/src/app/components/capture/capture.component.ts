55ee22e89f2ca8af3ab2817fad2f3fbdaa8ed648
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-11-09 17:10:11 +0530

    dashboard menu

--- WaveMasterFrontend/src/app/components/capture/capture.component.ts
+++ WaveMasterFrontend/src/app/components/capture/capture.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./capture.component.scss']
 })
 export class CaptureComponent {
+    start: boolean = true
+    openAccordion: string = 'collapseOne';
 
+    // Toggle accordion items
+    toggleAccordion(accordionId: string): void {
+      this.openAccordion = this.openAccordion === accordionId ? this.openAccordion : accordionId;
+    }
+  
+    // Check if an accordion item is open
+    isAccordionOpen(accordionId: string): boolean {
+      return this.openAccordion === accordionId;
+    }
+
+    toggleStartStop(){
+      this.start = !this.start
+    }
 }
