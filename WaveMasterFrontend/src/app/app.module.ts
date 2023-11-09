fb688bc4fb3e77f2830f06bcc40ad2c39fc3689c
sanjay <sanjaysunil34@gmail.com>
2023-11-09 15:09:12 +0530

    Components folder creation

--- WaveMasterFrontend/src/app/app.module.ts
+++ WaveMasterFrontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
