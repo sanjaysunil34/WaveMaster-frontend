2905314490093199c61a78e3d236164055acfa4d
sanjay <sanjaysunil34@gmail.com>
2023-11-13 14:39:53 +0530

    test services done

---
+++ WaveMasterFrontend/src/app/services/test.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+  baseUrl: string = "http://localhost:3000"
+
+  httpHeader={
+    headers:new HttpHeaders({
+      'Content-Type': 'application/json',      
+    })
+  }
+
+  constructor(private httpClient:HttpClient) { }
+
+  testComponent(command: string) : Observable<string>{
+    return this.httpClient.post<string>(this.baseUrl + '/test', JSON.stringify(command), this.httpHeader)
+    .pipe(
+      catchError(this.httpError)
+    );
+  }
+
+  httpError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      msg = error.error.message;
+    } else {
+      msg = 'Error Code:${error.status}\nMessage:${error.message}';
+    }
+    console.log(msg);
+    return throwError(msg);
+  }
+}
