9a3f1f2fdb35bcb1ada0f1efc70460e9b0ab4e87
sanjay <sanjaysunil34@gmail.com>
2023-11-27 15:38:46 +0530

    Tab disable, disconnect button, tab icon

--- WaveMasterFrontend/src/app/components/capture/capture.component.ts
+++ WaveMasterFrontend/src/app/components/capture/capture.component.ts
@@ -126,7 +126,7 @@ export class CaptureComponent implements OnDestroy{
         this.captureDataSubscription.unsubscribe();
         this.captureControlDataSubscription.unsubscribe();
       }
-      
+      this.captureEvent.emit(this.isCaptureOn);
     }
 
     onXScaleChange(event: any){
