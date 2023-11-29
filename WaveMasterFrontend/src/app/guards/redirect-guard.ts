3e70c1a57932fb9965da2baf9f3f1c9de7d7a8bf
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-11-29 09:41:39 +0530

    refactored guards

--- WaveMasterFrontend/src/app/guards/redirect-guard.ts
+++ WaveMasterFrontend/src/app/guards/redirect-guard.ts
@@ -1,8 +1,8 @@
-import { ActivatedRouteSnapshot, CanActivateFn, Router } from "@angular/router";
+import { CanActivateFn } from "@angular/router";
 import { inject } from '@angular/core';
 import { RedirectService } from "../services/redirect.service";
 
 export const RedirectGuard: CanActivateFn = (route, state) => {    
-    const redirectService = inject(RedirectService);
+    inject(RedirectService);
     return true;
 }
