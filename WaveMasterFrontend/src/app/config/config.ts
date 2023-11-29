e7cc572595672d3f8f7ccd9ca4332a165b7b2057
sanjay <sanjaysunil34@gmail.com>
2023-11-29 10:42:03 +0530

    Config commented

--- WaveMasterFrontend/src/app/config/config.ts
+++ WaveMasterFrontend/src/app/config/config.ts
@@ -1,7 +1,12 @@
 import { HttpHeaders } from "@angular/common/http";
 
+//Base URL for sending all the http requests are defined.
 export const BASE_URL = "http://localhost:3000";
 
+/**
+ * Http header to be sent in all the http requests are defined here.
+ * @returns The http header.
+ */
 export const httpHeader = () => {
     return {
         headers:new HttpHeaders({
